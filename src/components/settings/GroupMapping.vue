2b5a0cc58b5a4cfba302953874771f07b3fe0cae
Paweł Kotiuk <kotiuk@zohomail.eu>
2024-02-18 00:49:26 +0100

    Testy

--- src/components/settings/GroupMapping.vue
+++ src/components/settings/GroupMapping.vue
@@ -3,7 +3,18 @@
     {{ t(appName, 'Add group mapping') }}
   </button>
   <div v-for="(mapping, mappingIdx) in groupMapping" :key="mapping">
+  <!-- Only for discord checklist with names-->
+  <template v-if="appName === 'discord'">
+    <select class="foreign-group" v-model="mapping.foreign">
+      <option v-for="(name, id) in getGroupsForDiscord()" :key="id" :value="id">
+        {{ name }}
+      </option>
+    </select>
+  <!-- else -->
+  </template>
+  <template v-else>
     <input type="text" class="foreign-group" v-model="mapping.foreign" />
+  </template>
     <select class="local-group" :name="mapping.foreign ? inputNamePrefix + '['+mapping.foreign+']' : ''">
       <option v-for="group in groups" :key="group" :value="group" :selected="mapping.local === group">
         {{ group }}
@@ -17,6 +28,12 @@
 export default {
   props: ['groups', 'groupMapping', 'inputNamePrefix']
 }
+
+// Function to get names and IDs of all groups_for_discord as dictionary
+function getGroupsForDiscord() {
+  return {131232:"Role1"};
+}
+
 </script>
 
 <style scoped>
